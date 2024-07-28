ae8ff3dcbb4fd3744725cc1bb63405f1de45eba0
Red Crossbones <gastonehuen@gmail.com>
2024-07-28 14:06:56 -0300

    Update main.py

--- Cata-Aulas/app/src/main.py
+++ Cata-Aulas/app/src/main.py
@@ -1,5 +1,27 @@
 import subprocess
 from categorias.Edificio import Edificio
+import os
+import shutil
+
+
+def mover_archivos_a_carpeta(carpeta_destino):
+    # Crear la carpeta si no existe
+    if not os.path.exists(carpeta_destino):
+        os.makedirs(carpeta_destino)
+        print(f"Carpeta '{carpeta_destino}' creada.")
+    else:
+        print(f"Carpeta '{carpeta_destino}' ya existe.")
+
+    # Obtener la lista de archivos en el directorio actual
+    archivos = os.listdir('.')
+
+    # Mover archivos CSV y JSON a la carpeta destino
+    for archivo in archivos:
+        if archivo.endswith('.csv') or archivo.endswith('.json'):
+            ruta_origen = os.path.join('.', archivo)
+            ruta_destino = os.path.join(carpeta_destino, archivo)
+            shutil.move(ruta_origen, ruta_destino)
+            print(f"Archivo '{archivo}' movido a '{carpeta_destino}'.")
 
 
 def main():
@@ -24,6 +46,9 @@ def main():
 
         # Corre el programa de asignación de materias
         subprocess.run(["python", "app/src/asignacion6.py"], check=True)
+
+        # Llamar a la función para mover los archivos
+        mover_archivos_a_carpeta('archivos generados')
 
     except Exception as e:
         print(f"Error: {e}")
